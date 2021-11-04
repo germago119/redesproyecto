bb37ebcf11724dc6d5b9a17b4466c7f491e3695e
germago <roger.andres18@gmail.com>
2021-11-04 11:43:45 -0600

    fix body parser

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-//import { getAll } from './functions.js';
 import { Database } from './Database.js';
 
 const app = express()
@@ -20,50 +19,50 @@ app.listen(port, () => {
 })
 // ----------------------------------------------------------------------------------------
 
-app.get('/music', async(req, res) => {    
-  const jsonResponse = await db.getDbData();  
+app.get('/music', async(req, res) => {
+  const jsonResponse = await db.getDbData();
   res.send(jsonResponse);
 })
 
-app.post('/login', async(req, res) => { 
+app.post('/login', async(req, res) => {
   const { username,password } = req.body;
   const resultado = await db.logInUser(username,password);
-  res.send(resultado); 
+  res.send(resultado);
 })
 
 // Para agregar una nueva cancion
-app.post('/newSong', async(req, res) => { 
+app.post('/newSong', async(req, res) => {
   const { URL,Album,Artista,NombreCancion,Letra,Status } = req.body;
   await db.insertData(URL,Album,Artista,NombreCancion,Letra,Status);
-  res.send(JSON.stringify("{Agregado: CORRECTO}")); 
+  res.send(JSON.stringify("{Agregado: CORRECTO}"));
 })
 
-app.post('/letra', async(req, res) => { 
+app.post('/letra', async(req, res) => {
   const { URL } = req.body;
   const letra_response = await db.getLetra(URL);
-  res.send(letra_response); 
+  res.send(letra_response);
 })
 
-app.post('/unaCancion', async(req, res) => { 
+app.post('/unaCancion', async(req, res) => {
   const { _id } = req.body;
   const cancion_response = await db.getUnaCancion(_id);
-  res.send(cancion_response); 
+  res.send(cancion_response);
 })
 
-app.post('/saveEditedSong', async(req, res) => { 
+app.post('/saveEditedSong', async(req, res) => {
   const { _id,URL,Album,Artista,NombreCancion,Letra } = req.body;
   await db.updateEditedSong(_id,URL,Album,Artista,NombreCancion,Letra);
-  res.send(JSON.stringify("{Agregado: CORRECTO}")); 
+  res.send(JSON.stringify("{Agregado: CORRECTO}"));
 })
 
-app.post('/filtrado', async(req, res) => { 
+app.post('/filtrado', async(req, res) => {
   const { dato, filtro } = req.body;
   const data_response = await db.getSongsByFilter(dato,filtro);
-  res.send(data_response); 
+  res.send(data_response);
 })
 
-app.delete('/borrar', async(req, res) => { 
+app.delete('/borrar', async(req, res) => {
   const { _id } = req.body;
   await db.deleteSongsById(_id);
-  res.send(JSON.stringify("{Eliminado: CORRECTO}")); 
+  res.send(JSON.stringify("{Eliminado: CORRECTO}"));
 })
