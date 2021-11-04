0ab9d4123bc34324dc2df27f0ea8f4b332ea0da8
germago <roger.andres18@gmail.com>
2021-11-04 12:50:50 -0600

    format

--- Database.js
+++ Database.js
@@ -1,12 +1,12 @@
-import { MongoClient } from 'mongodb';
+import {MongoClient} from 'mongodb';
 import mongoose from 'mongoose';
 
 export class Database {
-    
+
     constructor() {
-      this.url = 'mongodb+srv://vicarhz:Hola1.@cluster0.wlbav.mongodb.net/KaraokeDB?retryWrites=true&w=majority';
-      this.client = new MongoClient(this.url);
-      this.dbName = 'KaraokeDB';
+        this.url = 'mongodb+srv://vicarhz:Hola1.@cluster0.wlbav.mongodb.net/KaraokeDB?retryWrites=true&w=majority';
+        this.client = new MongoClient(this.url);
+        this.dbName = 'KaraokeDB';
     }
 
     async connectDB() {
@@ -16,148 +16,133 @@ export class Database {
 
     async getDbData() {
         const db = this.client.db(this.dbName);
-        const collection = db.collection('Canciones');        
-        const findResult = await collection.find({}).toArray();
-        //console.log('Found documents =>', findResult);
-        return findResult
+        const collection = db.collection('Canciones');
+        return collection.find({}).toArray()
     }
-    
-    async logInUser(user, pass){
-      const db = this.client.db(this.dbName);
-      const collection = db.collection('Users'); 
-      const findResult = await collection.find(
-        {
-          $and:
-            [
-              {"usuario": user},
-              {"Password": pass}
-            ]
-        }).toArray();
-        //console.log(findResult.length)
-        if(findResult.length == 0){
-          return findResult
-        }
-        else{
-          const out = {
-            "NombreUsuario": findResult[0].NombreUsuario,
-            "usuario": findResult[0].usuario,
-            "TipoUsuario": findResult[0].TipoUsuario
-          };
-        
-        return out
+
+    async logInUser(user, pass) {
+        const db = this.client.db(this.dbName);
+        const collection = db.collection('Users');
+        const findResult = await collection.find(
+            {
+                $and:
+                    [
+                        {"usuario": user},
+                        {"Password": pass}
+                    ]
+            }).toArray();
+        if (findResult.length === 0) {
+            return findResult
+        } else {
+            return {
+                "NombreUsuario": findResult[0].NombreUsuario,
+                "usuario": findResult[0].usuario,
+                "TipoUsuario": findResult[0].TipoUsuario
+            }
         }
     }
 
     // Insertar nuevo documento
-    async insertData(URL,Album,Artista,NombreCancion,Letra,Status) {
-      const db = this.client.db(this.dbName);
-      const collection = db.collection('Canciones'); 
-      const myobj = { 
-        "URL": URL,
-        "Album": Album,
-        "Artista": Artista,
-        "NombreCancion": NombreCancion,
-        "Letra": Letra,
-        "Status": Status
-      };    
-      await collection.insertOne(myobj);            
+    async insertData(URL, Album, Artista, NombreCancion, Letra, Status) {
+        const db = this.client.db(this.dbName);
+        const collection = db.collection('Canciones');
+        const myobj = {
+            "URL": URL,
+            "Album": Album,
+            "Artista": Artista,
+            "NombreCancion": NombreCancion,
+            "Letra": Letra,
+            "Status": Status
+        };
+        await collection.insertOne(myobj);
     }
 
     async getLetra(URL) {
-      const db = this.client.db(this.dbName);
-      const collection = db.collection('Canciones'); 
-      const findResult = await collection.find(
-        {
-          $and:
-            [
-              {"URL": URL}
-            ]
-        }).toArray();
-      const out = {
-        "Letra": findResult[0].Letra,       
-      };
-      return out;
+        const db = this.client.db(this.dbName);
+        const collection = db.collection('Canciones');
+        const findResult = await collection.find(
+            {
+                $and:
+                    [
+                        {"URL": URL}
+                    ]
+            }).toArray();
+        return {
+            "Letra": findResult[0].Letra,
+        };
     }
 
     async getUnaCancion(_id) {
-      const db = this.client.db(this.dbName);
-      const collection = db.collection('Canciones'); 
-      const myObj = 
-      {
-        "_id": new mongoose.Types.ObjectId(_id)
-      }    
-      const findResult = await collection.find(myObj).toArray();          
-      return findResult;
+        const db = this.client.db(this.dbName);
+        const collection = db.collection('Canciones');
+        const myObj =
+            {
+                "_id": new mongoose.Types.ObjectId(_id)
+            }
+        return collection.find(myObj).toArray();
     }
 
-    async updateEditedSong(_id,URL,Album,Artista,NombreCancion,Letra) {
-      const db = this.client.db(this.dbName);
-      const collection = db.collection('Canciones'); 
-      const valor =  true;
-      var myobj = { $set:{ 
-        "URL": URL,
-        "Album": Album,
-        "Artista": Artista,
-        "NombreCancion": NombreCancion,
-        "Letra": Letra,
-        "Status": "Active"    
-      }};    
-      await collection.updateOne({"_id":mongoose.Types.ObjectId(_id)},myobj);            
+    async updateEditedSong(_id, URL, Album, Artista, NombreCancion, Letra) {
+        const db = this.client.db(this.dbName);
+        const collection = db.collection('Canciones');
+        const myobj = {
+            $set: {
+                "URL": URL,
+                "Album": Album,
+                "Artista": Artista,
+                "NombreCancion": NombreCancion,
+                "Letra": Letra,
+                "Status": "Active"
+            }
+        };
+        await collection.updateOne({"_id": mongoose.Types.ObjectId(_id)}, myobj);
     }
 
-    async getSongsByFilter(dato,filtro) {
-      const db = this.client.db(this.dbName);
-      const collection = db.collection('Canciones'); 
-      let myObj;
-      let busquedaPorLetra = false;
-      switch (filtro) {
-        case "NombreCancion":
-          myObj = {
-            "NombreCancion": dato
-          }   
-          break;
-        case "Artista":
-          myObj = {
-            "Artista": dato
-          } 
-          break;
-        case "Album":
-          myObj = {
-            "Album": dato
-          } 
-          break;
-        default: //Case Fragmento de la Letra
-          myObj = {
-            "Letra" : {$regex: dato }
-          } 
-          busquedaPorLetra = true;
-          break;
-      }
+    async getSongsByFilter(dato, filtro) {
+        const db = this.client.db(this.dbName);
+        const collection = db.collection('Canciones');
+        let myObj;
+        switch (filtro) {
+            case "NombreCancion":
+                myObj = {
+                    "NombreCancion": dato
+                }
+                break;
+            case "Artista":
+                myObj = {
+                    "Artista": dato
+                }
+                break;
+            case "Album":
+                myObj = {
+                    "Album": dato
+                }
+                break;
+            default: //Case Fragmento de la Letra
+                myObj = {
+                    "Letra": {$regex: dato}
+                }
+                break;
+        }
 
-      let findResult;
-      if (busquedaPorLetra){
-        //console.log(myObj);
-        findResult = await collection.find(myObj).toArray();          
-      }
-      else{
-        findResult = await collection.find(myObj).toArray();          
-      } 
-      return findResult;
+        let findResult;
+        findResult = await collection.find(myObj).toArray();
+
+        return findResult;
     }
-    
+
     async deleteSongsById(_id) {
-      const db = this.client.db(this.dbName);
-      const collection = db.collection('Canciones'); 
-      const myObj = 
-      {
-        "_id": new mongoose.Types.ObjectId(_id)
-      }    
-      await collection.deleteOne(myObj);      
-    }    
-    //db.Canciones.deleteOne({_id:ObjectId("616a038723a72bc3e9bf7c87")})
+        const db = this.client.db(this.dbName);
+        const collection = db.collection('Canciones');
+        const myObj =
+            {
+                "_id": new mongoose.Types.ObjectId(_id)
+            }
+        await collection.deleteOne(myObj);
+    }
 
     async closeConnection() {
-      this.client.close();
+        await this.client.close();
     }
-  }
+}
 
--- app.js
+++ app.js
@@ -1,85 +1,81 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import { Database } from './Database.js';
+import {Database} from './Database.js';
 
 const app = express()
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.set("port", process.env.PORT || 3001);
 app.set("host", process.env.HOST || "localhost");
 
-app.listen(app.get("port"), () => {
-  console.log(
-      "%s server listening at http://%s:%s",
-      process.env.NODE_ENV,
-      app.get("host"),
-      app.get("port")
-  );
-});
-
-const s = "Server Running... || Host: " + app.get("host") +" || Port: " + app.get("port")
-
 app.get("/", (req, res) => {
-  res.send(s)
+    res.send(s)
 });
 
 const db = new Database();
 await db.connectDB();
 
 // ----------------------------------------------------------------------------------------
-// app.listen(port, () => {
-//   console.log(`Example app listening at http://localhost:${port}\n`)
-// })
+app.listen(app.get("port"), () => {
+    console.log(
+        "%s server listening at http://%s:%s",
+        process.env.NODE_ENV,
+        app.get("host"),
+        app.get("port")
+    );
+});
+
+const s = "Server Running... || Host: " + app.get("host") + " || Port: " + app.get("port")
 // ----------------------------------------------------------------------------------------
 
-app.get('/music', async(req, res) => {
-  const jsonResponse = await db.getDbData();
-  res.send(jsonResponse);
+app.get('/music', async (req, res) => {
+    const jsonResponse = await db.getDbData();
+    res.send(jsonResponse);
 })
 
-app.post('/login', async(req, res) => {
-  const { username,password } = req.body;
-  const resultado = await db.logInUser(username,password);
-  res.send(resultado);
+app.post('/login', async (req, res) => {
+    const {username, password} = req.body;
+    const resultado = await db.logInUser(username, password);
+    res.send(resultado);
 })
 
 // Para agregar una nueva cancion
-app.post('/newSong', async(req, res) => {
-  const { URL,Album,Artista,NombreCancion,Letra,Status } = req.body;
-  await db.insertData(URL,Album,Artista,NombreCancion,Letra,Status);
-  res.send(JSON.stringify("{Agregado: CORRECTO}"));
+app.post('/newSong', async (req, res) => {
+    const {URL, Album, Artista, NombreCancion, Letra, Status} = req.body;
+    await db.insertData(URL, Album, Artista, NombreCancion, Letra, Status);
+    res.send(JSON.stringify("{Agregado: CORRECTO}"));
 })
 
-app.post('/letra', async(req, res) => {
-  const { URL } = req.body;
-  const letra_response = await db.getLetra(URL);
-  res.send(letra_response);
+app.post('/letra', async (req, res) => {
+    const {URL} = req.body;
+    const letra_response = await db.getLetra(URL);
+    res.send(letra_response);
 })
 
-app.post('/unaCancion', async(req, res) => {
-  const { _id } = req.body;
-  const cancion_response = await db.getUnaCancion(_id);
-  res.send(cancion_response);
+app.post('/unaCancion', async (req, res) => {
+    const {_id} = req.body;
+    const cancion_response = await db.getUnaCancion(_id);
+    res.send(cancion_response);
 })
 
-app.post('/saveEditedSong', async(req, res) => {
-  const { _id,URL,Album,Artista,NombreCancion,Letra } = req.body;
-  await db.updateEditedSong(_id,URL,Album,Artista,NombreCancion,Letra);
-  res.send(JSON.stringify("{Agregado: CORRECTO}"));
+app.post('/saveEditedSong', async (req, res) => {
+    const {_id, URL, Album, Artista, NombreCancion, Letra} = req.body;
+    await db.updateEditedSong(_id, URL, Album, Artista, NombreCancion, Letra);
+    res.send(JSON.stringify("{Agregado: CORRECTO}"));
 })
 
-app.post('/filtrado', async(req, res) => {
-  const { dato, filtro } = req.body;
-  const data_response = await db.getSongsByFilter(dato,filtro);
-  res.send(data_response);
+app.post('/filtrado', async (req, res) => {
+    const {dato, filtro} = req.body;
+    const data_response = await db.getSongsByFilter(dato, filtro);
+    res.send(data_response);
 })
 
-app.delete('/borrar', async(req, res) => {
-  const { _id } = req.body;
-  await db.deleteSongsById(_id);
-  res.send(JSON.stringify("{Eliminado: CORRECTO}"));
+app.delete('/borrar', async (req, res) => {
+    const {_id} = req.body;
+    await db.deleteSongsById(_id);
+    res.send(JSON.stringify("{Eliminado: CORRECTO}"));
 })
